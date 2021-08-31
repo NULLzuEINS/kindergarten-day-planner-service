02406b7f8c9e9ca341378ba84630179a6142976b
André Lademann <vergissberlin@googlemail.com>
2021-08-31 23:19:49 +0200

    Add routes in swagger configuration file

--- routes/root.js
+++ routes/root.js
@@ -2,58 +2,95 @@
 
 module.exports = async function (fastify, opts) {
 
-  // Register the plugin before your routes
+  /*
   fastify.register(require('fastify-swagger'), {
-    exposeRoute: true,
-    routePrefix: '/',
-    swagger: require('../schema.json')
+    routePrefix: '/documentation',
+    swagger: {
+      info: {
+        title: 'Kita day planner swagger',
+        description: 'API to generate kita day planner pdf',
+        version: '0.0.1'
+      },
+      externalDocs: {
+        url: 'https://github.com/nullzueins/kindergarten-day-planner-service/',
+        description: 'Find more info on GitHub repository'
+      },
+      host: '127.0.0.1:3000',
+      schemes: ['http'],
+      consumes: ['application/json'],
+      produces: ['application/pdf'],
+      tags: [
+        {name: 'image', description: 'Day planner related images'},
+        {name: 'code', description: 'Code related end-points'}
+      ],
+      definitions: {
+        Image: {
+          type: 'object',
+          required: ['id'],
+          properties: {
+            id: {type: 'string', format: 'uuid'},
+            fileName: {type: 'string'},
+            description: {type: 'string'}
+          }
+        }
+      }
+    },
+    uiConfig: {
+      docExpansion: 'full',
+      deepLinking: true
+    },
+    staticCSP: true,
+    transformStaticCSP: (header) => header,
+    exposeRoute: true
+  })
+  */
+  
+  fastify.register(require('fastify-swagger'), {
+    mode: 'static',
+    specification: {
+      path: './swagger.yaml'
+    },
+    exposeRoute: true
   })
 
+  fastify.get('/status', async (request, reply) => {
+    return {
+      "health": request.query.health,
+      "date": new Date().toISOString()
+    }
+  })
 
-  fastify.post('/pdf/:id', {
+  fastify.get('/', {
     schema: {
-      description: 'post some data',
-      tags: ['user', 'code'],
-      summary: 'qwerty',
+      description: 'Get the root path',
+      tags: ['test'],
+      summary: 'Get the root path',
       params: {
         type: 'object',
         properties: {
-          id: {
+          name: {
             type: 'string',
-            description: 'user id'
-          }
-        }
-      },
-      body: {
-        type: 'object',
-        properties: {
-          obj: {
-            type: 'object',
-            properties: {
-              some: {type: 'string'}
-            }
+            description: 'The name of the user'
           }
         }
       },
       response: {
-        201: {
-          description: 'Successful response',
+        200: {
           type: 'object',
           properties: {
-            hello: {type: 'string'}
+            hello: {
+              type: 'string',
+              description: 'The message'
+            }
           }
         }
-      },
-      security: [
-        {
-          "apiKey": []
-        }
-      ]
+      }
     }
-  }, (req, reply) => { })
+  }, (request, reply) => { })
 
   fastify.ready(err => {
     if (err) throw err
     fastify.swagger()
   })
+
 }
